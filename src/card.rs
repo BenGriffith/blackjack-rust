702d9906262aaa7eeaecf5ca17d133801b17c36d
Ben Griffith <bengriffith@outlook.com>
2026-07-29 21:57:26 -0600

    add Display trait for Suit and FaceCard

--- src/card.rs
+++ src/card.rs
@@ -1,8 +1,21 @@
+use std::fmt;
+
 pub enum Suit {
     Clubs,
     Diamonds,
     Hearts,
     Spades,
+}
+
+impl fmt::Display for Suit {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Suit::Clubs => write!(f, "Clubs"),
+            Suit::Diamonds => write!(f, "Diamonds"),
+            Suit::Hearts => write!(f, "Hearts"),
+            Suit::Spades => write!(f, "Spades"),
+        }
+    }
 }
 
 pub enum FaceCard {
@@ -11,13 +24,23 @@ pub enum FaceCard {
     King,
 }
 
+impl fmt::Display for FaceCard {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            FaceCard::Jack => write!(f, "Jack"),
+            FaceCard::Queen => write!(f, "Queen"),
+            FaceCard::King => write!(f, "King"),
+        }
+    }
+}
+
 pub struct Card {
-    suit: String,
-    rank: String,
+    pub suit: String,
+    pub rank: String,
 }
 
 impl Card {
-    fn card_value(&self) -> usize {
+    pub fn card_value(&self) -> usize {
         unimplemented!();
     }
 }
