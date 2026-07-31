c154be0b96c5991ecae03d9f2d036a615c6f0d5a
Ben Griffith <bengriffith@outlook.com>
2026-07-30 18:00:18 -0600

    refactor cards variables

--- src/deck.rs
+++ src/deck.rs
@@ -4,20 +4,31 @@ pub struct Deck {
     pub cards: Vec<Card>,
 }
 
+impl Default for Deck {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl Deck {
     pub fn new() -> Self {
         let suits: Vec<Suit> = vec![Suit::Clubs, Suit::Diamonds, Suit::Hearts, Suit::Spades];
-        let facecards: Vec<FaceCard> = vec![FaceCard::Jack, FaceCard::Queen, FaceCard::King];
-        let deck_suits: Vec<String> = suits.iter().map(|x: &Suit| x.to_string()).collect();
-        let deck_facecards: Vec<String> =
-            facecards.iter().map(|x: &FaceCard| x.to_string()).collect();
+        let face_cards: Vec<FaceCard> = vec![FaceCard::Jack, FaceCard::Queen, FaceCard::King];
+        let base_cards: Vec<String> = (1..=10).map(|c: usize| c.to_string()).collect();
 
+        let cards: Vec<String> = face_cards
+            .iter()
+            .map(|card: &FaceCard| card.to_string())
+            .chain(base_cards)
+            .collect();
+
+        let deck_suits: Vec<String> = suits.iter().map(|s: &Suit| s.to_string()).collect();
         let deck_cards: Vec<Card> = deck_suits
             .iter()
             .flat_map(|suit: &String| {
-                deck_facecards.iter().map(move |facecard: &String| Card {
+                cards.iter().map(move |card: &String| Card {
                     suit: suit.to_string(),
-                    rank: facecard.to_string(),
+                    rank: card.to_string(),
                 })
             })
             .collect();
