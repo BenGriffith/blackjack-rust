62e4a86018d8376a0380e0ac83c9b58449aa7b50
Ben Griffith <bengriffith@outlook.com>
2026-07-30 21:16:05 -0600

    reorganized helper functions

--- src/card.rs
+++ src/card.rs
@@ -34,13 +34,35 @@ impl fmt::Display for FaceCard {
     }
 }
 
+#[derive(Debug, Clone)]
 pub struct Card {
     pub suit: String,
     pub rank: String,
 }
 
 impl Card {
-    pub fn card_value(&self) -> usize {
+    pub fn get_suits() -> Vec<String> {
+        let suits: Vec<Suit> = vec![Suit::Clubs, Suit::Diamonds, Suit::Hearts, Suit::Spades];
+        let deck_suits: Vec<String> = suits.iter().map(|s: &Suit| s.to_string()).collect();
+        deck_suits
+    }
+
+    pub fn get_cards() -> Vec<String> {
+        let face_cards: Vec<FaceCard> = vec![FaceCard::Jack, FaceCard::Queen, FaceCard::King];
+        let mut playing_cards: Vec<String> = (2..=10).map(|c: usize| c.to_string()).collect();
+        let ace: String = String::from("Ace");
+        playing_cards.push(ace);
+
+        let cards: Vec<String> = face_cards
+            .iter()
+            .map(|card: &FaceCard| card.to_string())
+            .chain(playing_cards)
+            .collect();
+
+        cards
+    }
+
+    pub fn get_card_value() -> usize {
         unimplemented!();
     }
 }
--- src/deck.rs
+++ src/deck.rs
@@ -1,5 +1,4 @@
-use crate::card::{Card, FaceCard, Suit};
-use std::collections::HashMap;
+use crate::card::Card;
 
 #[derive(Debug, Clone)]
 pub struct Deck {
@@ -13,10 +12,15 @@ impl Default for Deck {
 }
 
 impl Deck {
-    pub fn new(&self) -> Self {
-        let cards = self.get_cards();
-        let deck_suits = self.get_suits();
-        let deck_cards: Vec<Card> = deck_suits
+    pub fn new() -> Self {
+        let cards: Vec<String> = Card::get_cards();
+        let deck_suits: Vec<String> = Card::get_suits();
+        let deck_cards: Vec<Card> = Self::set_deck(&deck_suits, &cards);
+        Self { cards: deck_cards }
+    }
+
+    fn set_deck(suits: &[String], cards: &[String]) -> Vec<Card> {
+        let deck_cards: Vec<Card> = suits
             .iter()
             .flat_map(|suit: &String| {
                 cards.iter().map(move |card: &String| Card {
@@ -25,35 +29,6 @@ impl Deck {
                 })
             })
             .collect();
-        Self { cards: deck_cards }
-    }
-
-    fn get_suits(&self) -> Vec<String> {
-        let suits: Vec<Suit> = vec![Suit::Clubs, Suit::Diamonds, Suit::Hearts, Suit::Spades];
-        let deck_suits: Vec<String> = suits.iter().map(|s: &Suit| s.to_string()).collect();
-        deck_suits
-    }
-
-    fn get_cards(&self) -> Vec<String> {
-        let face_cards: Vec<FaceCard> = vec![FaceCard::Jack, FaceCard::Queen, FaceCard::King];
-        let mut playing_cards: Vec<String> = (2..=10).map(|c: usize| c.to_string()).collect();
-        let ace: String = String::from("Ace");
-        playing_cards.push(ace);
-
-        let cards: Vec<String> = face_cards
-            .iter()
-            .map(|card: &FaceCard| card.to_string())
-            .chain(playing_cards)
-            .collect();
-
-        cards
-    }
-
-    fn get_card_value(&self) -> usize {
-        unimplemented!();
-    }
-
-    fn set_deck(&self) -> Vec<Card> {
-        unimplemented!();
+        deck_cards
     }
 }
