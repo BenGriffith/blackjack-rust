9c482c9f6bd1a558493020c8ca853d1340503b1d
Ben Griffith <bengriffith@outlook.com>
2026-08-01 09:59:36 -0600

    add Display trait for CardError

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,14 @@
-// use std::fmt;
+use std::fmt;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum CardError {
-    InvalidNumber,
+    InvalidNumber { value: String },
+}
+
+impl fmt::Display for CardError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            CardError::InvalidNumber { value } => write!(f, "Invalid number: {}", value),
+        }
+    }
 }
