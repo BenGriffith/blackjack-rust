e9eb9598bbedc3280a598d8d13da78ca60877141
Ben Griffith <griffithbenjamin@icloud.com>
2026-08-13 17:15:47 -0600

    remove error variant

--- src/error.rs
+++ src/error.rs
@@ -3,7 +3,6 @@ use std::io;
 
 #[derive(Debug)]
 pub enum GameError {
-    InvalidScore,
     InvalidBet,
     InvalidResponse,
     InvalidNumber,
@@ -20,7 +19,6 @@ impl From<io::Error> for GameError {
 impl fmt::Display for GameError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            GameError::InvalidScore => write!(f, "value should be greater than 0"),
             GameError::InvalidBet => write!(f, "please provide a positive integer value"),
             GameError::InvalidResponse => write!(f, "please enter an acceptable value"),
             GameError::InvalidNumber => write!(f, "please enter a valid number"),
